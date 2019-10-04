e96b5965d41ecc67e9337043e1c217c3a82b6968
Naoki Saruta <katsusand1@gmail.com>
2019-10-04 15:28:55 +0900

    Use Ricty in terminal

--- .hyper.js
+++ .hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 13,
 
     // font family with optional fallbacks
-    fontFamily: 'Monaco',
+    fontFamily: 'Ricty for Powerline',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
