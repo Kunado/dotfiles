821e35492fe1a2be54c95438d4e454a51ef4a451
Naoki Saruta <katsusand1@gmail.com>
2019-11-02 15:15:27 +0900

    Add zsh plugins and automate installation of plugins

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -2,3 +2,15 @@ source $ZPLUG_HOME/init.zsh
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
 zplug "zsh-users/zsh-syntax-highlighting"
+zplug "zsh-users/zsh-history-substring-search"
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-completions"
+zplug "chrissicool/zsh-256color"
+
+if ! zplug check --verbose; then
+  printf "Install? [y/N]: "
+  if read -q; then
+    echo; zplug install
+  fi
+fi
+zplug load --verbose
