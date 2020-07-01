dc40466b9ef45b98859feb3651758b414cedbb89
naoki saruta <katsusand1@gmail.com>
2020-07-01 13:00:18 +0900

    Set up installing with mitamae

---
+++ init.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+# based on https://github.com/akito19/dotfiles/blob/master/init.sh
+# based on https://github.com/akito19/dotfiles/blob/master/init.sh
+
+set -eu
+
+is_setup() {
+  echo "Do you setup $1? [y/N]"
+  while :
+  do
+    read -r answer
+    case $answer in
+      'yes' | 'y') return 0 ;;
+      [nN]o | [nN]) return 1 ;;
+      *) echo "Try again because you input incorrect letter. Do you setup $1? [y/N]" ;;
+    esac
+  done
+}
+
+
+if is_setup 'global .gitignore'; then
+  if [ "$(uname)" = 'Darwin' ]; then
+    curl -fL 'https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore' >> ~/.gitignore
+  else
+    curl -fL 'https://raw.githubusercontent.com/github/gitignore/master/Global/Linux.gitignore' >> ~/.gitignore
+  fi
+
+  echo '' >> ~/.gitignore
+  echo '# VisualStudioCode' >> ~/.gitignore
+  curl -fL 'https://raw.githubusercontent.com/github/gitignore/master/Global/VisualStudioCode.gitignore' >> ~/.gitignore
+  echo '' >> ~/.gitignore
+  echo '# Vim' >> ~/.gitignore
+  curl -fL 'https://raw.githubusercontent.com/github/gitignore/master/Global/Vim.gitignore' >> ~/.gitignore
+fi
+
+if [ "$(uname)" = 'Darwin' ]; then
+  if is_setup 'Xcode Command Line Tools'; then
+    xcode-select --install
+  fi
+
+  if is_setup 'Homebrew'; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew doctor
+  fi
+
+  if is_setup 'some tools by Homebrew and Homebrew cask'; then
+    ~/dotfiles/brew.sh
+    ~/dotfiles/cask.sh
+  fi
+fi
+
+if [ ! -d $HOME/dotfiles ]; then
+  git clone https://github.com/tkmru/dotfiles.git ~/dotfiles
+fi
+
+~/dotfiles/bin/setup_mitamae.sh
+~/dotfiles/deploy.sh
