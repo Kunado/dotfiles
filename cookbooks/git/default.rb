1a1ca7ddb58075baaacffa41241625fb09af5f55
naoki saruta <katsusand1@gmail.com>
2020-07-04 11:16:42 +0900

    Add sample roles and cookbooks

---
+++ cookbooks/git/default.rb
@@ -0,0 +1,11 @@
+package 'git'
+
+if node[:platform] == 'darwin'
+	file(gitconfig = File.join(ENV['HOME'], '.gitconfig')) do
+	  action :delete
+	  only_if { File.file?(gitconfig) && !File.symlink?(gitconfig) }
+	end
+end
+
+dotfile '.gitconfig'
+dotfile '.gitignore'
---
+++ roles/darwin/default.rb
@@ -0,0 +1,5 @@
+include_role 'base'
+
+include_cookbook 'git'
+
+dotfile 'tmux.conf'
