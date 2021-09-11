1871416509b9145871b53e0d57b55642dcc58d65
naoki saruta <katsusand1@gmail.com>
2021-09-11 09:51:40 +0900

    fixup! Add zellij cookbook

--- cookbooks/zellij/default.rb
+++ cookbooks/zellij/default.rb
@@ -5,5 +5,5 @@ github_binary 'zellij' do
   repository 'zellij-org/zellij'
   version 'v0.16.0'
   asset "zellij-x86_64-#{build}.tar.gz"
-  binary_path "zellij-x86_64-#{build}.tar.gz"
+  binary_path "zellij"
 end
