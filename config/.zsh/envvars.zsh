a99c58acda92d34dd1b53f8500f311a696f50f95
naoki saruta <katsusand1@gmail.com>
2022-08-16 01:00:49 +0900

    Update .gitconfig

--- config/.zsh/envvars.zsh
+++ config/.zsh/envvars.zsh
@@ -46,6 +46,7 @@ export PATH=/usr/local/opt/ruby/bin:$PATH
 export PATH=/usr/local/lib/ruby/gems/2.6.0/bin/:$PATH
 export PATH=/usr/local/opt/openssl/bin/:$PATH
 export PATH=$HOME/.tmux/bin:$PATH
+export PATH=$HOME/.asdf/shims:$PATH
 
 
 ### OTHER PATH
